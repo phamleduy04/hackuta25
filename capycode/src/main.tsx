5cc427e49226ff3384b8ea50781c1a0532160ea9
Duy Pham <32657584+phamleduy04@users.noreply.github.com>
2025-10-04 14:13:15 -0500

    routing

--- capycode/src/main.tsx
+++ capycode/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import { ConvexReactClient } from "convex/react";
 import "./index.css";
-import App from "./App.tsx";
 import { ConvexProviderWithAuth0 } from "convex/react-auth0";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { StrictMode, Suspense } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  BrowserRouter,
+  useRoutes,
+} from 'react-router-dom';
+import routes from '~react-pages';
+
+// eslint-disable-next-line react-refresh/only-export-components
+function App() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      {useRoutes(routes)}
+    </Suspense>
+  )
+}
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 createRoot(document.getElementById("root")!).render(
@@ -19,7 +32,9 @@ createRoot(document.getElementById("root")!).render(
       cacheLocation="localstorage"
     >
       <ConvexProviderWithAuth0 client={convex}>
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
       </ConvexProviderWithAuth0>
     </Auth0Provider>
   </StrictMode>,
--- capycode/vite.config.ts
+++ capycode/vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import pages from "vite-plugin-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    pages({
+      dirs: 'src/pages',
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
