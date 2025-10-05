93bb615f118624a59816e6097aa469152d093e81
ch1kim0n1 <chikimoni61@gmail.com>
2025-10-05 10:00:40 -0500

    character final

--- capycode/src/pages/code/index.tsx
+++ capycode/src/pages/code/index.tsx
@@ -1,12 +1,10 @@
 
 import CodeEditor from "./code-editor";
-import SpeakingCapy from "@/components/SpeakingCapy";
 
 export default function Code() {
     return (
         <div className="h-screen w-full relative">
             <CodeEditor />
-            <SpeakingCapy />
         </div>
     );
 }
